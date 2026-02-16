dd1934944cdc6cff645ce98b190c5415d9774c8c
Saikiran Biradar <saikiranbiradar76642@gmail.com>
2026-02-17 00:17:15 +0530

    first commit

---
+++ src/test/java/com/ecomm/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ecomm.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
